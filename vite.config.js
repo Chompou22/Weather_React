7117285e243330490f83886d516bb5ecc67bc868
028 <anbschool0028@028s-Mac-mini.local>
2024-01-10 17:30:33 +0700

    Config to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -9,4 +8,10 @@ export default defineConfig({
       external: ['@reduxjs/toolkit'], // Add other external dependencies as needed
     },
   },
+  resolve: {
+    alias: {
+      '@reduxjs/toolkit': '@reduxjs/toolkit',
+    },
+  },
+  // other configurations...
 });
