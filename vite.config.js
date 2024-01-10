d225dafaf666d5813212a081dc9cfee57e56c998
028 <anbschool0028@028s-Mac-mini.local>
2024-01-10 18:46:57 +0700

    rebuild project for deployment 5th

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@reduxjs/toolkit'], // Add other external dependencies as needed
-    },
-  },
-  resolve: {
-    alias: {
-      '@reduxjs/toolkit': '@reduxjs/toolkit',
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@reduxjs/toolkit'], // Add other external dependencies as needed
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@reduxjs/toolkit': '@reduxjs/toolkit',
+  //   },
+  // },
   // other configurations...
 });
